6717def3be6cf70dd774d1384c21b0cebf9525db
corradosantoro <santoro@dmi.unict.it>
2016-05-27 08:45:30 +0200

    example fixed

--- samples/factorial.py
+++ samples/factorial.py
@@ -13,7 +13,7 @@ class fact(Reactor):
     pass
 
 def factorial():
-    +fact("N") >> [ +fact(1, "N") ]
+    +fact("N") >> [ +fact("N", 1) ]
     +fact(1, "X") >> [ show("the resulting factorial is "), show_line("X") ]
     +fact("N", "X") >> [ "Y = int(N) * int(X)", "N = int(N) - 1", +fact("N", "Y") ]
 
